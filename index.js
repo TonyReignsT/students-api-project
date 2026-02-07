869283c0ee20e45a85dab8bf9bb572d21e232803
TonyReignsT <antonyochieng00@gmail.com>
2026-02-07 22:37:38 +0300

    Fixed error on oauth

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require("dotenv").config();
 // const {MongoClient} = require('mongodb');
-const MongoStore = require("connect-mongo");
+const MongoStore = require("connect-mongo").default;
 
 
 
